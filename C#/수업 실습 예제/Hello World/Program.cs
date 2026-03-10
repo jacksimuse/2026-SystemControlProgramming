c16b2e9220512f477797e41592d036f24f0cef2e
jacksimuse <cjh930306@naver.com>
2026-03-10 12:03:55 +0900

    Add initial Hello World program in C#

---
+++ C#/수업 실습 예제/Hello World/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World // 새 프로젝트, 프로젝트 이름으로 설정하면 나옴
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
