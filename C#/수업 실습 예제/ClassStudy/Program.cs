131a81705e11b36844faf0f792d1f38643250a8a
jacksimuse <cjh930306@naver.com>
2026-04-28 11:28:40 +0900

    Update Program.cs

--- C#/수업 실습 예제/ClassStudy/Program.cs
+++ C#/수업 실습 예제/ClassStudy/Program.cs
@@ -9,17 +9,39 @@ namespace ClassStudy
     internal class Program
     {
         // 공간 1 (class 안)
+        // 필드(전역변수), 메서드, 생성자 정의하는 / 들어가는 공간
 
         static void Main(string[] args)
         {
-            // 공간 2 (method 안)
-            Student student1 = new Student("자기 이름", 123456); // 선언과 초기화
-            // class명 변수명 = new 생성자
-            student1.Introduce();
+            // 공간 2 (method, 생성자 안)
+            // 정의되어있는 클래스나 메서드, 전역변수(필드)를 사용하는 공간
 
-            Student student2; // 선언
-            student2 = new Student("학생 2", 789123);
-            student2.Introduce();
+            //Student student1 = new Student("자기 이름", 123456); // 선언과 초기화
+            //// class명 변수명 = new 생성자
+            //student1.Introduce();
+
+            //Student student2; // 선언
+            //student2 = new Student("학생 2", 789123);
+            //student2.Introduce();
+
+
+            // machine 2개 소환
+            // machine1,2를 생성하고 메서드 호출까지 하기
+            Machine machine1 = new Machine("로봇팔", "컨베이어");
+            //machine1.Move();
+            //machine1.Run();
+            machine1.Engine = "1";
+            Console.WriteLine(machine1.Engine);
+
+
+            Console.WriteLine(machine1.Power);
+            machine1.Power = "Off";
+            Console.WriteLine(machine1.Power);
+
+
+            Machine machine2 = new Machine("팔팔", "컨베이어2");
+            //machine2.Move();
+            //machine2.Run();
         }
     }
 
